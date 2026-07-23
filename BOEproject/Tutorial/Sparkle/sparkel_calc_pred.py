c831d1299ebbb9723ed76e9792f545329248e342
wanghaibo <3245944633@qq.com>
2026-07-23 16:13:27 +0800

    tutorial

--- BOEproject/Tutorial/Sparkle/sparkel_calc_pred.py
+++ BOEproject/Tutorial/Sparkle/sparkel_calc_pred.py
@@ -19,15 +19,15 @@ layerstructure = {
 
 # 读取刻蚀图像并转换为示例 sag 数据
 ####闪点
-# image_path = r"F:\PHD\GitFiles\diffractsim\BOEproject\sparkle0511\image1.png"  # 你的图片路径
+# image_path = r"F:\PHD\GitFiles\diffractsim\BOEproject\Tutorial\imagefiles\image1.png"  # 你的图片路径
 # sag = SparkleSimulator.jetimg2sag(image_path = image_path, v_min=-0.339*um, v_max=0.78*um)
-# image_path = r"F:\PHD\GitFiles\diffractsim\BOEproject\sparkle0511\image2.png"  # 你的图片路径
+# image_path = r"F:\PHD\GitFiles\diffractsim\BOEproject\Tutorial\imagefiles\image2.png"  # 你的图片路径
 # sag = SparkleSimulator.jetimg2sag(image_path = image_path, v_min=-0.503*um, v_max=1.095*um)
-image_path = r"/BOEproject/sparkle0511/image3.png"  # 你的图片路径
-sag = SparkleSimulator.jetimg2sag(image_path = image_path, v_min=-0.635*um, v_max=1.301*um)
-# image_path = r"F:\PHD\GitFiles\diffractsim\BOEproject\sparkle0511\image4.png"  # 你的图片路径
-# sag = SparkleSimulator.jetimg2sag(image_path = image_path, v_min=-0.925*um, v_max=0.705*um)
-# image_path = r"F:\PHD\GitFiles\diffractsim\BOEproject\sparkle0511\image5.png"  # 你的图片路径
+# image_path = r"F:\PHD\GitFiles\diffractsim\BOEproject\Tutorial\imagefiles/image3.png"  # 你的图片路径
+# sag = SparkleSimulator.jetimg2sag(image_path = image_path, v_min=-0.635*um, v_max=1.301*um)
+image_path = r"F:\PHD\GitFiles\diffractsim\BOEproject\Tutorial\imagefiles\image4.png"  # 你的图片路径
+sag = SparkleSimulator.jetimg2sag(image_path = image_path, v_min=-0.925*um, v_max=0.705*um)
+# image_path = r"F:\PHD\GitFiles\diffractsim\BOEproject\Tutorial\imagefiles\image5.png"  # 你的图片路径
 # sag = SparkleSimulator.jetimg2sag(image_path = image_path, v_min=-0.731*um, v_max=0.69*um)
 
 sag = SparkleSimulator.mosaic_array(sag,5)
@@ -48,12 +48,12 @@ Isparkle = sim.get_Sparkle_origin(spectrum='G', layer_Structure=layerstructure, 
 sim.plot_intensity(Isparkle, square_root= False, units = um, text = 'Sparkle Pattern at RGB layer(unfilter)',colormap = 'gray')
 
 # # #####################Visual photo###################
-# Ivisual = sim.VisualPhoto(sigma= 62*um)
-# sim.plot_intensity(Ivisual, square_root= False, units = um, text = 'Visual Photo of Sparkle Pattern',colormap = 'gray')
-#
+Ivisual = sim.VisualPhoto(sigma= 62*um)
+sim.plot_intensity(Ivisual, square_root= False, units = um, text = 'Visual Photo of Sparkle Pattern',colormap = 'gray')
+
 # #
 # # #################### SIM method ######################
-sparkle_sim = sim.SIM(method = "A")#  A:对应黄光AG B:对应普通蒙砂刻蚀AG
+sparkle_sim = sim.SIM(method = "B")#  A:对应黄光AG B:对应普通蒙砂刻蚀AG
 ##########调取sim_calibration.jion中的拟合参数，进行闪点预测##########
 import json
 
